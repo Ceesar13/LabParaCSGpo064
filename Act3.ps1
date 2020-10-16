60974fdf48bb199c95817ddbaa45785dd49b56d6
Ceesar13 <72643304+Ceesar13@users.noreply.github.com>
2020-10-16 14:15:45 -0500

    Add files via upload

---
+++ Act3.ps1
@@ -0,0 +1,12 @@
+﻿
+Function Buscaip{
+  Get-NetIPConfiguration
+}
+Function Buscatcp{
+ Get-NetTCPConnection 
+
+}
+Write-Output("Nombre: César Alejandro Rodríguez Pérez - Matricula: 1734223")
+Write-Output("Materia: Lab.ParaCS - Horario: Sabado 12:00-2:00 pm")
+Buscaip  
+Buscatcp
